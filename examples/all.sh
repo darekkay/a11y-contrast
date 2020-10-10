7f93b9cd1fa60e6ef555b150b06e4a424d53e46c
Darek Kay <darekkay@gmail.com>
2020-10-10 14:25:09 +0200

    Include Tailwind 2.0 proposal palette
    Defined in https://github.com/tailwindlabs/tailwindcss/pull/2132

--- examples/all.sh
+++ examples/all.sh
@@ -4,4 +4,5 @@
 ../bin/a11y-contrast.js ibm-v2.1.json --min-ratio-3=40 --min-ratio-4.5=50 --min-ratio-7=70
 ../bin/a11y-contrast.js open-color.json
 ../bin/a11y-contrast.js tailwind.json
+../bin/a11y-contrast.js tailwind-v2-proposal.json
 ../bin/a11y-contrast.js uswds.json

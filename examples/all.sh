36fb0ae898954902640fec00488e6847a596e2d7
Darek Kay <hello@darekkay.com>
2020-11-23 21:18:42 +0100

    Update Tailwind 2.0 color palette

--- examples/all.sh
+++ examples/all.sh
@@ -3,6 +3,7 @@
 ../bin/a11y-contrast.js ibm-v1.json
 ../bin/a11y-contrast.js ibm-v2.1.json --min-ratio-3=40 --min-ratio-4.5=50 --min-ratio-7=70
 ../bin/a11y-contrast.js open-color.json
-../bin/a11y-contrast.js tailwind.json
+../bin/a11y-contrast.js tailwind-v1.json
 ../bin/a11y-contrast.js tailwind-v2-proposal.json
+../bin/a11y-contrast.js tailwind-v2.json
 ../bin/a11y-contrast.js uswds.json
